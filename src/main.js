39e807662b15e9037f1d10992e54c95392a3769c
Bob <bobwu2100@gmail.com>
2019-10-24 23:52:57 +0800

    动态组件component

--- src/main.js
+++ src/main.js
@@ -1,57 +1,29 @@
 import Vue from './lib/vue.js';
-import ButtonComponent from './component/ButtonComponent.vue';
-import Hello from './component/Hello.vue';
-import Assign from './component/Assign.vue';
-import FlyTable from './component/FlyTable.vue';
 import './css/style.css';
-
+let Home = {
+  template: `
+    <p style="color:#787878;">Hello Home!</p>
+  `
+}
+let About = {
+  template: `
+    <p>Hello About!</p>
+  `
+}
 let vm = new Vue({
   el: '#app',
   data(){
     return {
-      fields: [
-        {
-          label: '名称',
-          prop: 'name'
-        },
-        {
-          label: '数量',
-          prop: 'quantity'
-        },
-        {
-          label: '价格',
-          prop: 'price'
-        },{
-          label: '',
-          prop: 'operate'
-        },
-      ],
-      goods: [
-        {
-          name: '苹果',
-          quantity: 200,
-          price: 6.8,
-          isMarked: false
-        },
-        {
-          name: '西瓜',
-          quantity: 50,
-          price: 4.8,
-          isMarked: false
-        },
-        {
-          name: '榴莲',
-          quantity: 0,
-          price: 22.8,
-          isMarked: false
-        }
-      ]
+      view: 'Home'
     }
   },
-  components: {
-    ButtonComponent,
-    Hello,
-    Assign,
-    FlyTable
+  methods:{
+    toggleView(view){
+      this.view = view;
+    }
+  },
+  components:{
+    Home,
+    About
   }
 });
